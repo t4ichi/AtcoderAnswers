9f94b16166762ce33725b33d149ca6a14a8d6016
t4ichi <taichinoheya020316@icloud.com>
2022-12-18 19:00:24 +0900

    abc273

--- abc273/b.cpp
+++ abc273/b.cpp
@@ -1,5 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main(){
+    ll x;
+    int k;
+    cin >> x >> k;
+
+    ll d = 1;
+    for(int i = 0;i < k;i++){
+        ll a = x / d % 10;
+        
+        d *= 10;
+        x /= d;
+        x *= d;
+        if(a >= 5){
+            x += d;
+        }
+    }
+    cout << x << "\n";
 }
--- abc273/c.cpp
+++ abc273/c.cpp
@@ -2,4 +2,28 @@
 using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    map<int,int> mp;
+
+    for(int i = 0;i < n;i++){
+        int a;
+        cin >> a;
+        mp[a]++;
+    }
+    vector<int> ans;
+    for(auto i : mp){
+        ans.push_back(i.second);
+    }
+    reverse(ans.begin(),ans.end());
+
+    for(int i = 0;i < n;i++){
+        if(ans.size() > i){
+            cout << ans[i] << "\n";
+        }
+        else{
+            cout << 0 << "\n";
+        }
+    }
 }
