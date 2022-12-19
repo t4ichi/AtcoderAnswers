1a0d4b425229b152e24fc4b17b713f264219101a
t4ichi <taichinoheya020316@icloud.com>
2022-12-20 01:28:50 +0900

    abc275

--- abc275/d.cpp
+++ abc275/d.cpp
@@ -4,7 +4,8 @@ using ll = long long;
 
 ll func(ll n ,map<ll,ll> &memo){
     if(memo[n] != 0) return memo[n];
-    return memo[n] = func()
+    if(n == 0) return 1;
+    return memo[n] = func(n/2,memo) + func(n/3,memo);
 }
 
 int main(){
